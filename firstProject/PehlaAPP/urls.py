d26a60e6913eeed4fa1acdd4b63148fb132d9020
RahulYadav08 <rkrrkumar511@gmail.com>
2024-09-26 15:57:44 +0530

    Django login required implemented in firstProject

--- firstProject/PehlaAPP/urls.py
+++ firstProject/PehlaAPP/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
 
-    path("", greet)
+    path("", greet),
+    
 ]
--- firstProject/firstProject/middlewares.py
+++ firstProject/firstProject/middlewares.py
@@ -28,11 +28,11 @@ class CustomMiddleware():
 
         return response
 
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        pass
+    # def process_view(self, request, view_func, view_args, view_kwargs):
+    #     pass
 
-    def process_exception(self, request, exception):
-        pass
+    # def process_exception(self, request, exception):
+    #     pass
 
-    def process_template_response(self, request, response):
-        pass
+    # def process_template_response(self, request, response):
+        # pass
